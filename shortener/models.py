8dd84c870436393114ac2bc0f16454756ac7be06
janpc01 <janpc01@gmail.com>
2025-07-22 15:17:15 -0600

    Added default short code generation

--- shortener/models.py
+++ shortener/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from .utils import generate_code
 
 class ShortURL(models.Model):
     original_url = models.URLField()
-    short_code = models.CharField(max_length=6, unique=True)
+    short_code = models.CharField(max_length=6, unique=True, default=generate_code)
 
     def __str__(self):
         return f"{self.short_code} -> {self.original_url}"
