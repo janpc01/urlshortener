f5c0e7352630e7d3612d1814d06dbbbe24da84d1
janpc01 <janpc01@gmail.com>
2025-07-22 15:06:37 -0600

    Created ShortURL model

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class ShortURL(models.Model):
+    original_url = models.URLField()
+    short_code = models.CharField(max_length=6, unique=True)
+
+    def __str__(self):
+        return f"{self.short_code} -> {self.original_url}"
